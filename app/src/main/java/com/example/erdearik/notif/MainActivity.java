5b73407e9d110220b8157b047a9df79c85f9f5be
ARDHI MAARIK <ardhi_maarik12u@student.ipb.ac.id>
2016-02-22 04:41:08 +0700

    first init

---
+++ app/src/main/java/com/example/erdearik/notif/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.erdearik.notif;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+    int numMessages = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Notify("You've received new message", "message");
+            }
+        });
+    }
+
+
+    private void Notify(String notificationTitle, String notificationMessage){
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.drawable.ic_android_24dp);
+        builder.setContentTitle(notificationTitle);//"Notifications Example")
+        builder.setContentText(notificationMessage);//"This is a test notification");
+        builder.setTicker("New Message Alert!");
+        builder.setSmallIcon(R.drawable.ic_system_update_24dp);
+
+        builder.setNumber(++numMessages);
+
+   /* Add Big View Specific Configuration */
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+
+        String[] events = new String[6];
+        events[0] = new String("This is first line....");
+        events[1] = new String("This is second line...");
+        events[2] = new String("This is third line...");
+        events[3] = new String("This is 4th line...");
+        events[4] = new String("This is 5th line...");
+        events[5] = new String("This is 6th line...");
+
+        // Sets a title for the Inbox style big view
+        inboxStyle.setBigContentTitle("Big Title Details:");
+
+        // Moves events into the big view
+        for (int i=0; i < events.length; i++) {
+            inboxStyle.addLine(events[i]);
+        }
+        builder.setStyle(inboxStyle);
+
+        Intent notificationIntent = new Intent(this, NotificationView.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+        // Add as notification
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(1, builder.build());
+    }
+
+    // Remove notification
+    private void removeNotification() {
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.cancel(1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/erdearik/notif/NotificationView.java
@@ -0,0 +1,15 @@
+package com.example.erdearik.notif;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by erdearik on 2/22/16.
+ */
+public class NotificationView extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
